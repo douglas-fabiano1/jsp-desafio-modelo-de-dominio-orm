16ef4a0a515302efeeb5486930505929b93bb393
Douglas Fabiano <contatodouglasfabiano1@gmail.com>
2023-11-09 18:34:11 -0300

    Project Spring Start

---
+++ src/main/java/com/devsuperior/jspdesafio2/JspDesafio2Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.jspdesafio2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspDesafio2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JspDesafio2Application.class, args);
+	}
+
+}
